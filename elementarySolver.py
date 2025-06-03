451c638afe0b6b72d751fd8672e0757eded6dfd2
Timon Dubreuil <timondubreuil@outlook.com>
2025-06-03 09:31:41 +0200

    configs elementaires aléatoires ok

--- elementarySolver.py
+++ elementarySolver.py
@@ -1,10 +1,37 @@
 import random
+from typing import Set
 
 from util import coversAll, isElementary
 
 def solveElementary(M,N,sensors):
-    sortedSensors = [key for key in sensors]
-    random.shuffle(sortedSensors)
 
-    print("covers all ? : ",coversAll(M,N,sensors,sortedSensors))
-    print("elementary ? : ",isElementary(M,N,sensors,sortedSensors))
+    for i in range(100):
+        config = generateElementary(M,N,sensors)
+
+        print("---------------------------------------------------")
+        print(config)
+        print("covers all ? : ",coversAll(M,N,sensors,config))
+        print("elementary ? : ",isElementary(M,N,sensors,config))
+        print("---------------------------------------------------")
+
+def generateElementary(M,N,sensors):
+    """génère une config aléatoire élémentaire."""
+    chosen : Set = set()
+
+    while not (coversAll(M,N,sensors,chosen)):
+
+        randomSensorIndex = random.randint(1,N)
+
+        chosen.add(f"s{randomSensorIndex}")
+
+    while not isElementary(M,N,sensors,chosen):
+        copy = [s for s in chosen]
+
+        randomSensorIndex = random.randint(0,len(copy)-1)
+
+        copy.pop(randomSensorIndex)
+
+        if coversAll(M,N,sensors,copy):
+            chosen = copy
+
+    return chosen
